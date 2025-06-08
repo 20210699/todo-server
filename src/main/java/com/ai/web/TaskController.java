9b5620b7a2a1d12f716624fd6cfe11389b254c9f
20210699 <102691173+20210699@users.noreply.github.com>
2025-06-09 00:29:06 +0900

    Feat: 조회 API 기능 구현(#1) (#2)

--- src/main/java/com/ai/web/TaskController.java
+++ src/main/java/com/ai/web/TaskController.java
@@ -1,15 +1,16 @@
 package com.ai.web;
 
+import com.ai.constants.TaskStatus;
 import com.ai.model.Task;
 import com.ai.service.TaskService;
 import com.ai.web.vo.TaskRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -30,4 +31,46 @@ public class TaskController {
         return ResponseEntity.ok(res);
     }
 
+    /**
+     * 특정 마감일에 해당하는 할 일 목록 반환
+     *
+     * @param dueDate 할 일의 마감일
+     * @return 마감일에 해당하는 할 일 목록
+     */
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasksByDueDate(Optional<String> dueDate) {
+        List<Task> res;
+
+        if (dueDate.isPresent()) {
+            res = this.taskService.getByDueDate(dueDate.get());
+        } else {
+            res = this.taskService.getAll();
+        }
+
+        return ResponseEntity.ok(res);
+    }
+
+    /**
+     * 특정 ID에 해당하는 할 일을 조회
+     *
+     * @param id 할 일 ID
+     * @return ID에 해당하는 할 일 객체
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable Long id) {
+        var res = this.taskService.getOne(id);
+        return ResponseEntity.ok(res);
+    }
+
+    /**
+     * 특정 상태에 해당하는 할 일 목록을 반화
+     *
+     * @param status 할 일 상태
+     * @return 상태에 해당하는 할 일 목록
+     */
+    @GetMapping("status/{status}")
+    public ResponseEntity<List<Task>> getTasksByStatus(@PathVariable TaskStatus status) {
+        var res = this.taskService.getAllByStatus(status);
+        return ResponseEntity.ok(res);
+    }
 }
