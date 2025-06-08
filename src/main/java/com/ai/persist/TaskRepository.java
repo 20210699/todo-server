cb7028490a06b9b5f0b2ccb6cbcc45f903bc7747
20210699 <20210699@duksung.ac.kr>
2025-06-09 00:26:08 +0900

    Feat: 조회 API 기능 구현(#1)

--- src/main/java/com/ai/persist/TaskRepository.java
+++ src/main/java/com/ai/persist/TaskRepository.java
@@ -1,7 +1,15 @@
 package com.ai.persist;
 
+import com.ai.constants.TaskStatus;
+import com.ai.model.Task;
 import com.ai.persist.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    List<TaskEntity> findAllByDueDate(Date dueDate);
+    List<TaskEntity> findAllByStatus(TaskStatus status);
 }
--- src/main/java/com/ai/service/TaskService.java
+++ src/main/java/com/ai/service/TaskService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -28,6 +30,33 @@ public class TaskService {
 
         var saved = this.taskRepository.save(e);
         return entityToObject(saved);
+    }
+
+    public List<Task> getAll() {
+        return this.taskRepository.findAll().stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getByDueDate(String dueDate) {
+        return this.taskRepository.findAllByDueDate(Date.valueOf(dueDate)).stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getAllByStatus(TaskStatus status) {
+        return this.taskRepository.findAllByStatus(status).stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public Task getOne(Long id) {
+        return this.entityToObject(this.getById(id));
+    }
+
+    private TaskEntity getById(Long id) {
+        return this.taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(String.format("not exists task id [%d]", id)));
     }
 
     private Task entityToObject(TaskEntity e) {
